1c5da9d23c1a1c965b4fb931bc7bd7b2ab93c249
charcodes24 <charcodes24@gmail.com>
2021-08-22 11:05:05 -0700

    Completed assignment.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,19 +2,23 @@ class AuthorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    authors = Author.all 
-    render json: authors
+    authors = Author.all
+    render json: authors, include: ['profile', 'posts', 'posts.tags']
   end
 
   def show
-    author = Author.find(params[:id])
-    render json: author
+    author = find_author
+    render json: author, include: ['profile', 'posts', 'posts.tags']
   end
 
   private
 
   def render_not_found_response
     render json: { error: "Author not found" }, status: :not_found
+  end
+
+  def find_author
+    Author.find(params[:id])
   end
 
 end
